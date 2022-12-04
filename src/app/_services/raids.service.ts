7c393ceb53f99fdb4e5e40875a9e29a85d8c9d1e
Farah404 <96427557+Farah404@users.noreply.github.com>
2022-12-04 21:58:19 +0100

    get all raids list

---
+++ src/app/_services/raids.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Raid } from 'src/model/raids';
+
+const baseUrl = 'http://localhost:8080/api/raids';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RaidsService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Raid[]> {
+    return this.http.get<Raid[]>(baseUrl);
+  }
+
+  get(id: any): Observable<Raid> {
+    return this.http.get<Raid>(`${baseUrl}/${id}`);
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+
+  
+  update(id: any, data: any): Observable<any> {
+    return this.http.put(`${baseUrl}/${id}`, data);
+  }
+
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+
+  deleteAll(): Observable<any> {
+    return this.http.delete(baseUrl);
+  }
+
+  findByGuildName(raidName: any): Observable<Raid[]> {
+    return this.http.get<Raid[]>(`${baseUrl}?raidName=${raidName}`);
+  }
+}
