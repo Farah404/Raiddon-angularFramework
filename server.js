b130307989bb160666a0b898beaa3f8f43fee287
Farah404 <96427557+Farah404@users.noreply.github.com>
2023-01-03 12:38:04 +0100

    heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/mdb-angular-ui-kit-free'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/mdb-angular-ui-kit-free/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/mdb-angular-ui-kit-free', 'index.html'));
 });
 
 app.listen(process.env.PORT || 4200);
