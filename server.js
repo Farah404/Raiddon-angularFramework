3f7097cf1f9a4925c330a6bde35dda25f999d075
Farah404 <96427557+Farah404@users.noreply.github.com>
2023-01-03 11:06:06 +0100

    express server creation

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(__dirname + "/dist/mdb-angular-ui-kit-free"));
+
+app.get("/*", function (req, res) {
+  res.sendFile(
+    path.join(__dirname + "/dist/mon-projet/mdb-angular-ui-kit-free")
+  );
+});
